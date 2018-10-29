5ea4250bed5de9e5afed2e46f5a7467bb0f58552
Gustavo <guhpereira@outlook.com.br>
2018-10-29 09:06:30 -0300

    Code Start

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/home/gustavo/github/oguhpereira/side-left-template/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/home/gustavo/github/oguhpereira/side-left-template/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/home/gustavo/github/oguhpereira/side-left-template/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/home/gustavo/github/oguhpereira/side-left-template/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/home/gustavo/github/oguhpereira/side-left-template/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/home/gustavo/github/oguhpereira/side-left-template/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
+exports.data = () => import("/home/gustavo/github/oguhpereira/side-left-template/.cache/data.json")
+
